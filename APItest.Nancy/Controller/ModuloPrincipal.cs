bc3a4fa6dcae3ec17c74ab644aa6c63fd43e4455
Poxi <julianlastra.kz@gmail.com>
2021-11-11 17:44:29 -0300

    Post Implemented + Json

--- APItest.Nancy/Controller/ModuloPrincipal.cs
+++ APItest.Nancy/Controller/ModuloPrincipal.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Nancy;
+using Nancy.Extensions;
+using Newtonsoft.Json;
+using Properties;
 
 namespace APItest.Nancy.Controller
 {
@@ -12,27 +16,48 @@ namespace APItest.Nancy.Controller
         //Este constructor siempre lleva el mismo nombre que la clase
         public ModuloPrincipal()
         {
+            #region Tests + Status 
+
             Get("/v1/status", _ =>
             {
-
-                return "200: OK";
-
+                return "GET 200: OK";
             });
 
             Get("/v1/test", _ =>
             {
-
-                return "Test Reponse";
-
+                return "GET Test Reponse";
             });
 
             Post("/v1/test", _ =>
             {
-
-
-                return "";
-
+                return "POST Test Reponse";
             });
+
+            #endregion
+
+            #region Calculator
+
+            Post("/v1/calc", x =>
+            {
+                var product = new Product();
+                var rawRequest = Context.Request.Body.AsString();
+
+                //product.Name = "Papa";
+                //product.Price = 4;
+
+                //string output = JsonConvert.SerializeObject(product);
+
+                Product dProduct = JsonConvert.DeserializeObject<Product>(rawRequest);
+
+                dProduct.Price = dProduct.Price + 1;
+
+                string output = JsonConvert.SerializeObject(dProduct);
+
+                return output;
+            });
+
+            #endregion
+
         }
     }
 }
---
+++ Properties/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
